51c10cc4179cdae5fbe9ab7d1ea784f538157260
Micah Read <164657374+P5YC0DR3AM3R@users.noreply.github.com>
2024-06-03 16:33:52 -0400

    fixed cache

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -52,7 +52,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
