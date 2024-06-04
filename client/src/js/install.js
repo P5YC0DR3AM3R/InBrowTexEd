4ead22477bdbf15945f7927ac937982a8b8fe742
Micah Read <164657374+P5YC0DR3AM3R@users.noreply.github.com>
2024-06-04 15:13:54 -0400

    README.md update

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,21 +3,24 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt event fired');
     window.deferredPrompt = event;
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    console.log('Install button clicked');
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
         return;
     }
     promptEvent.prompt();
     const { outcome } = await promptEvent.userChoice;
-    console.log(outcome);
+    console.log('User choice outcome:', outcome);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+    console.log('App installed');
     window.alert('JATE has been installed');
 });
