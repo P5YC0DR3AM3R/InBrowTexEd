a3ed5349d38505a8fd3873b7c09bf3728c10b688
Micah Read <164657374+P5YC0DR3AM3R@users.noreply.github.com>
2024-06-14 09:25:26 -0400

    Tudor helped me fix the misplaced files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,11 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     console.log('beforeinstallprompt event fired');
     window.deferredPrompt = event;
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     console.log('Install button clicked');
     const promptEvent = window.deferredPrompt;
